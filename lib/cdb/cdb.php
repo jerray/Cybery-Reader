9065c73f8551a306ef1a609cbcc9e633b94d5d3d
Glove <glovenone@gmail.com>
2011-02-10 15:51:55 +0800

    Glove version 2

--- lib/cdb/cdb.php
+++ lib/cdb/cdb.php
@@ -1,140 +1,47 @@
 <?php
-
-class db_mysql
-{
-	var $querynum = 0; 	//µ±Ç°Ò³Ãæœø³Ì²éÑ¯ÊýŸÝ¿âµÄŽÎÊý
-	var $dblink;	//ÊýŸÝ¿âÁŽœÓ×ÊÔŽ
-
-	//ÁŽœÓÊýŸÝ¿â
-	function connect($dbhost, $dbuser, $dbpw, $dbname = "", $dbcharset = 'utf-8', $pconnect = 0, $halt = true)
-	{
-		$func = emtpy($pconnect) ? 'mysql_connect':'mysql_pconnect';	//Èç¹û$pconnectÎª¿ÕÔòÊ¹ÓÃ$connect
-		$this->dblink = @$func($dbhost, $dbuser, $dbpw);
-		if($halt && !$this->dblink)		//µ±mysqlÁ¬œÓÊ§°Üµ÷ÓÃ$this->halt:ÏÔÊŸŽíÎóÐÅÏ¢
-		{
-			$this->halt("ÎÞ·šÁŽœÓÊýŸÝ¿â!");
+	class CDB{
+		//打印出错信息
+		function halt($msg){
 		}
-	}
-	//ÉèÖÃ²éÑ¯×Ö·ûŒ¯
-	mysql_query("SET character_set_connection={$dbcharset}, character_set_results = {$dbcharset}, character_set_client = utf-8",/* $this->dblink */);
 
-	//Ñ¡ÔñÊýŸÝ¿â
-	function select_db($dbname)
-	{
-		return mysql_select_db($dbname, $this->link);
-	}
-	//ÖŽÐÐÊýŸÝ¿â
-	function query($sql)
-	{
-		$this->querynum++;
-		return mysql_query($sql, $this->dblink);
-	}
-
-	//×ªÒå×Ö·û
-	function escape($string) 
-	{
-		if(get_magic_quotes_gpc()) $string = stripslashes($string);
-		return mysql_real_escape_string($string);
-	}
-
-	//ŽÓÊýŸÝ±ítable_nameÖÐÈ¡³öÒ»ÌõŒÇÂŒ£¬Âú×ãÌõŒþ£º×Ö¶ÎÃûÎªfield_nameµÄ×Ö¶Î£¬ÆäÖµÎªvalue
-	function fetch($table_name, $field_name, $value)
-	{
-		$sql = select $field_name from $table_name where $field_name = $value;
-		$result = mysql_query($sql);
-		return mysql_fetch_object($result);
-	}
-
-	//ŽÓÊýŸÝ±ítable_nameÖÐÈ¡³öËùÓÐ·ûºÏÌõŒþconditionµÄŒÇÂŒ
-	function get($table_name, $condition)
-	{
-		return select * from $table_name where $condition;
-	}
-
-	//ÏòÊýŸÝ±ítable_nameÖÐ²åÈëÒ»ÌõŒÇÂŒ£¬dataÊÇÒ»žö¹ØÁªÊý×é£¬ŒüÃûÎª×Ö¶ÎÃû£¬ÖµÎª×Ö¶ÎµÄÖµ
-	function insert($table_name, $data)
-       	{
-		$q="INSERT INTO `".$this->pre.$table_name."` ";
-		$v=''; $n='';
-	
-		foreach($data as $key=>$val)
-	       	{
-			$n.="`$key`, ";
-			if(strtolower($val)=='null') $v.="NULL, ";
-			elseif(strtolower($val)=='now()') $v.="NOW(), ";
-			else $v.= "'".$this->escape($val)."', ";
+		//链接数据库
+		function connect($dbhost, $dbuser, $dbpw, $dbname ="", $dbcharset = 'utf-8', $pconnect = 0, $halt = true){
 		}
-	
-		$q .= "(". rtrim($n, ', ') .") VALUES (". rtrim($v, ', ') .");";
-	
-		if($this->query($q)){
-			return mysql_insert_id();
+
+		//选择数据库
+		function select_db($dbname){
 		}
-		else return false;
-	}
 		
-	//žüÐÂÊýŸÝ±ítable_nameÖÐµÄidÎªid_valueµÄŒÇÂŒ£¬dataÊÇÒ»žö¹ØÁªÊý×é£¬ŒüÃûÎª×Ö¶ÎÃû£¬ÖµÎª×Ö¶ÎµÄÖµ
-	function query_update($table_name, $data)
-	{
-		$q="UPDATE `".$this->pre.$table_name."` SET ";
-	
-		foreach($data as $key=>$val)
-	       	{
-			if(strtolower($val)=='null') $q.= "`$key` = NULL, ";
-			elseif(strtolower($val)=='now()') $q.= "`$key` = NOW(), ";
-			else $q.= "`$key`='".$this->escape($val)."', ";
-		}
-		$q = rtrim($q, ', ') . ' WHERE '.$where.';';
-	
-		return $this->query($q);
-	}
-
-	//ŸßÓÐ¿É±ä²ÎÊýžöÊýµÄº¯Êý£¬ÀàËÆÓÚsprintf£¬fsql¶šÒåÁËŸÝžñÊœ£¬v1, v2µÈ±äÁ¿¶šÒåÁËÒªÌæ»»µÄÖµ£¬È»ºóœ«Ìæ»»ºóµÄ×Ö·ûŽ®×÷ÎªÊýŸÝ¿â²éÑ¯œøÐÐÖŽÐÐ
-
-	
-	function queryf()
-	{
-		$pa = func_get_args();
-
-		$data = $pa[0] array();			///$pa[0]  žñÊœ......???????
-		for($i = 1; $i < func_num_args(); $i++)
-		{
-			$data[$i - 1] = $pa[$i];
-		}
-		foreach($data as $key=>$val)
-	       	{
-			if(strtolower($val)=='null') $q.= "`$key` = NULL, ";
-			elseif(strtolower($val)=='now()') $q.= "`$key` = NOW(), ";
-			else $q.= "`$key`='".$this->escape($val)."', ";
+		//执行数据库
+		function query($sql){
 		}
 
-		$q = "SELECT `". rtrim($q, ', ') ."` from ".$this->pre.$table_name."` " . ' WHERE '.$where.';';
+		//转义字符
+		function escape($string){
+		}
+
+		//从数据表table_name中取出一条记录，满足条件：字段名为field_name的字段，其值为value
+		function fetch($table_name, $field_name, $value){
+		}
+		
+		//从数据表table_name中取出所有符合条件condition的记录
+		function get($table_name, $condition){
+		}
+
+		//向数据表table_name中插入一条记录，data是一个关联数组，键名为字段名，值为字段的值
+		function insert($table_name, $data){
+		}
+
+		//更新数据表table_name中的id为id_value的记录，data是一个关联数组，键名为字段名，值为字段的值
+		function query_update($table_name, $data){
+		}
+
+		//具有可变参数个数的函数，类似于sprintf，fsql定义了据格式，v1, v2等变量定义了要替换的值，然后将替换后的字符串作为数据库查询进行执行
+		function queryf(){
+		}
+		
+		//关闭链接
+		function close(){
+		}
 	}
-
-	//¹Ø±ÕÁ¬œÓ
-	function close()
-	{
-		return mysql_close($this->dblink);
-	}
-
-
-}
-?>  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+?>
