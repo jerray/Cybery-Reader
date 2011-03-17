149e8a5ef8a7a87b6b857a5ffe7bf8de0e14d1b1
7lemon <7lemonsoul@gmail.com>
2011-03-17 21:02:49 +0800

    Move the outputs of post.class.php to post.html

--- actions/post.class.php
+++ actions/post.class.php
@@ -47,40 +47,7 @@ class Post extends Action
                 }
             }
 		}
-		
-		?>
-<div id="posts">	
-<h2 class="flip"><p><?php echo $this->PageData['feedname'];?></p></h2>
-<div id="panall">
-<?php if($this->PageData['isitem']):?>
-    <?php foreach($this->PageData['items'] as $item):?>
-        <div>
-            <h3 class="flip">
-                <img src="../static/images/starblank.png" class="star" />
-                <?php echo $item["title"]; ?>
-            </h3>
-            <div id="panel" class="panel" style="display:none;">
-                <? if ($item["content"]):?>
-                    <?php echo $item["content"]; ?>
-                <? else: ?>
-                    <?php echo $item["description"]; ?>
-                <? endif; ?>
-            </div>
-        </div>
-    <?php endforeach;?>
-<?php endif;?>
-</div>
-</div>
-<script type="text/javascript">
-	$("h3.flip").click(function(){
-		$(this).next("div.panel").slideToggle("slow");
-	});
-	$("img.star").click(function(){
-		$(this).attr("src","../static/images/starfull.png");
-	});
-</script>
-
-		<?php
+		$tpl->render('post.html', $this->PageData);
 	}
 };
 
